835e7375b803853b2b1ee1ad379de55b3757df7e
Oleg Semenov <66306538+semoleg1986@users.noreply.github.com>
2023-03-20 13:56:33 +0300

    Summary

---
+++ react-course/react-ts-components/src/components/Card.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ICard } from '../type';
+
+class Card extends React.Component<ICard> {
+  props: ICard;
+  
+  render() {
+    return (
+      <div className="post">
+        <img src={this.props.imageUrl} alt={this.props.title} />
+        <h2>{this.props.title}</h2>
+        <p>{this.props.description}</p>
+        <div className="post-price">{this.props.price}</div>
+      </div>
+    );
+  }
+}
+
+export { Card };
---
+++ react-course/react-ts-components/src/type.tsx
@@ -0,0 +1,7 @@
+export interface ICard {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+}
