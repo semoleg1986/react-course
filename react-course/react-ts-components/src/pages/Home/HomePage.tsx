244b6741feb22050a197f699b029031cae9174be
Oleg Semenov <semoleg1986@gmail.com>
2023-04-07 22:47:19 +0300

    Summary

--- react-course/react-ts-components/src/pages/Home/HomePage.tsx
+++ react-course/react-ts-components/src/pages/Home/HomePage.tsx
@@ -1,13 +1,8 @@
-import SearchBar from '../../components/SearchBar/SearchBar';
-import Cards from '../../components/Cards/Cards';
+// import SearchBar from '../../components/SearchBar/SearchBar';
+// import Cards from '../../components/Cards/Cards';
 
 const Home = () => {
-  return (
-    <>
-      <SearchBar />
-      <Cards />
-    </>
-  );
+  return <>Hello World</>;
 };
 
 export default Home;
