5b28545938f2f114d1ba4d1dd4f6bc0014bdb8ca
Oleg Semenov <semoleg1986@gmail.com>
2023-04-08 03:13:11 +0300

    Change style

--- react-course/react-ts-components/src/components/Cards/Cards.tsx
+++ react-course/react-ts-components/src/components/Cards/Cards.tsx
@@ -2,27 +2,18 @@ import React, { useEffect, useState } from 'react';
 import ApiService from '../../services/ApiServ';
 import Card from '../Card/Card';
 import './Cards.css';
-import axios from 'axios';
 import { IMovie } from '../Card/Card.props';
 
 const Cards = () => {
   const [movies, setMovies] = useState<IMovie[]>([]);
 
   useEffect(() => {
-    axios
-      .get('https://api.themoviedb.org/3/tv/popular?api_key=a5f9c29429f3ee32204af9b3691e7d08')
-      .then((response) => {
-        setMovies(response.data.results);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    const fetchMovies = async () => {
+      const { data } = await ApiService.get('tv/popular');
+      setMovies(data.results);
+    };
+    fetchMovies();
   }, []);
-  // const fetchMovies = async () => {
-  //   const { data } = await ApiService.get('tv/popular');
-  //   setMovies(data.results);
-  // };
-  // fetchMovies();
 
   return (
     <div className="post-container">
