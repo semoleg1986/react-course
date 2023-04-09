91864bbeb121712202ed161a425bae9212522b61
Oleg Semenov <semoleg1986@gmail.com>
2023-04-09 13:38:02 +0300

    Update App.tsx

--- react-course/react-ts-components/src/App.tsx
+++ react-course/react-ts-components/src/App.tsx
@@ -4,20 +4,17 @@ import Forms from './pages/Form/FormPage';
 import About from './pages/About/AboutPage';
 import NotFoundPage from './pages/404/NotFoundPage';
 import Layout from './components/Layout';
-import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="*" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="form" element={<Forms />} />
-          <Route path="about" element={<About />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="*" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="form" element={<Forms />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
 };
 export default App;
