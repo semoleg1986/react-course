eb7520e3ccc7214afb07dc649597124bce43d517
Oleg Semenov <semoleg1986@gmail.com>
2023-04-09 14:43:24 +0300

    Update App.tsx

--- react-course/react-ts-components/src/App.tsx
+++ react-course/react-ts-components/src/App.tsx
@@ -8,7 +8,7 @@ import Layout from './components/Layout';
 const App = () => {
   return (
     <Routes>
-      <Route path="/*" element={<Layout />}>
+      <Route path="*" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="form" element={<Forms />} />
         <Route path="about" element={<About />} />
