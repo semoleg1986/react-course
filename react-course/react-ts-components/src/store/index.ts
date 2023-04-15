cf1b796b939cc6e99fe28ee1ef6d2aea01161efc
Oleg Semenov <semoleg1986@gmail.com>
2023-04-15 17:51:31 +0300

    Summary

---
+++ react-course/react-ts-components/src/store/index.ts
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { movieApi } from './movieApi';
+
+export const store = configureStore({
+  reducer: {
+    [movieApi.reducerPath]: movieApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(movieApi.middleware),
+});
---
+++ react-course/react-ts-components/src/store/movieApi.ts
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { IMovie } from '../components/Card/Card.props';
+
+const API_KEY = 'a5f9c29429f3ee32204af9b3691e7d08';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const movieApi = createApi({
+  reducerPath: 'movieApi',
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  endpoints: (builder) => ({
+    getPopularMovies: builder.query<IMovie[], number>({
+      query: (page = 1) => ({
+        url: `/tv/popular`,
+        params: { page, api_key: API_KEY },
+      }),
+    }),
+    getMovieDetails: builder.query<IMovie, number>({
+      query: (movieId) => ({
+        url: `/tv/${movieId}`,
+        params: { api_key: API_KEY },
+      }),
+    }),
+  }),
+});
+
+export const { useGetPopularMoviesQuery, useGetMovieDetailsQuery } = movieApi;
