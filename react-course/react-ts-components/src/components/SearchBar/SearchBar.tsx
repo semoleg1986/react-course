331821b46597a234f54da27bca36eb8d7d75c35f
Oleg Semenov <semoleg1986@gmail.com>
2023-04-16 22:17:55 +0300

    Change SearchBar
    create css button
    update SearchBar

--- react-course/react-ts-components/src/components/SearchBar/SearchBar.tsx
+++ react-course/react-ts-components/src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './SearchBar.css';
 import { BsSearch } from 'react-icons/bs';
 
@@ -8,15 +8,34 @@ interface SearchBarProps {
 }
 
 const SearchBar = ({ searchQuery, onSearch }: SearchBarProps) => {
+  const [query, setQuery] = useState(searchQuery);
+  const [searchOnType, setSearchOnType] = useState(false);
+
+  useEffect(() => {
+    setQuery(searchQuery);
+  }, [searchQuery]);
+
   const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
     const value = event.currentTarget.value;
-    onSearch(value);
+    setQuery(value);
+    if (searchOnType) {
+      onSearch(value);
+    }
   };
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
-      onSearch(searchQuery);
+      event.preventDefault();
+      onSearch(query);
     }
+  };
+
+  const handleButtonClick = () => {
+    onSearch(query);
+  };
+
+  const toggleSearchOnType = () => {
+    setSearchOnType(!searchOnType);
   };
 
   return (
@@ -30,8 +49,15 @@ const SearchBar = ({ searchQuery, onSearch }: SearchBarProps) => {
           onChange={handleChange}
           onKeyPress={handleKeyPress}
           placeholder="Search bar"
-          value={searchQuery}
+          value={query}
         />
+        <button type="button" onClick={handleButtonClick}>
+          Search
+        </button>
+        <label>
+          <input type="checkbox" checked={searchOnType} onChange={toggleSearchOnType} />
+          Search on type
+        </label>
       </div>
     </div>
   );
