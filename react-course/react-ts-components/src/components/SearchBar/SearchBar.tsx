b9ba9e2e9c14999f2346de67aebbc6ecc4e497e9
Oleg Semenov <semoleg1986@gmail.com>
2023-04-20 10:08:37 +0300

    Update SearchBar.tsx

--- react-course/react-ts-components/src/components/SearchBar/SearchBar.tsx
+++ react-course/react-ts-components/src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './SearchBar.css';
 import { BsSearch } from 'react-icons/bs';
 
@@ -8,25 +8,14 @@ interface SearchBarProps {
 }
 
 const SearchBar = ({ searchQuery, onSearch }: SearchBarProps) => {
-  const [query, setQuery] = useState(searchQuery);
-  const [searchOnType] = useState(false);
-
-  useEffect(() => {
-    setQuery(searchQuery);
-  }, [searchQuery]);
-
   const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
     const value = event.currentTarget.value;
-    setQuery(value);
-    if (searchOnType) {
-      onSearch(value);
-    }
+    onSearch(value);
   };
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
-      event.preventDefault();
-      onSearch(query);
+      onSearch(searchQuery);
     }
   };
 
@@ -41,7 +30,7 @@ const SearchBar = ({ searchQuery, onSearch }: SearchBarProps) => {
           onChange={handleChange}
           onKeyPress={handleKeyPress}
           placeholder="Search bar"
-          value={query}
+          value={searchQuery}
         />
       </div>
     </div>
